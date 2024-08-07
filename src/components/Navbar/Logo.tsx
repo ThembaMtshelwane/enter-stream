2f30a967c9b28397fc4430c403c59fe0fec42c83
ThembaMtshelwane <thembamm3@gmail.com>
2024-08-07 15:46:13 +0200

    Navbar: Minor changes

--- src/components/Navbar/Logo.tsx
+++ src/components/Navbar/Logo.tsx
@@ -3,7 +3,7 @@ type Props = {
 };
 const Logo = ({ styles }: Props) => {
   return (
-    <a href="" className={`${styles} ml-[15%]`}>
+    <a href="#" className={`${styles} ml-[15%]`}>
       <p className="text-3xl  md:text-4xl ">
         <span className="text-blue-600 ">Enter-</span>
         <span>Steam</span>
