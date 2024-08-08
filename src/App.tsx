36d16787b223965e43fc2cd0e201f368c790a3ea
ThembaMtshelwane <thembamm3@gmail.com>
2024-08-08 11:12:29 +0200

    Layout: basic Mainlayout structure implemented

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,15 @@ import {
 } from "react-router-dom";
 
 import HomePage from "./pages/HomePage"; // Home page
+import MainLayout from "./layouts/MainLayout";
 
 export default function App() {
   const router = createBrowserRouter(
-    createRoutesFromElements(<Route index element={<HomePage />} />)
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+      </Route>
+    )
   ); // Creates router
   return <RouterProvider router={router} />;
 }
