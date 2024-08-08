0a5064bfe40d4c023ea8f6f0cb293a998e13972d
ThembaMtshelwane <thembamm3@gmail.com>
2024-08-08 12:59:04 +0200

    Routes: Logo home route updated

--- src/components/Navbar/Logo.tsx
+++ src/components/Navbar/Logo.tsx
@@ -1,15 +1,17 @@
+import { Link } from "react-router-dom";
+
 type Props = {
   styles: string;
   logoStyle: string;
 };
 const Logo = ({ styles, logoStyle }: Props) => {
   return (
-    <a href="/" className={`${styles} ml-[15%]`}>
+    <Link to="/" className={`${styles} ml-[15%]`}>
       <p className="text-3xl  md:text-4xl ">
         <span className={`${logoStyle} `}>Enter-</span>
         <span>Steam</span>
       </p>
-    </a>
+    </Link>
   );
 };
 
