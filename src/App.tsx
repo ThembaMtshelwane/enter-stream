8c094c96ca1b8c7cf168cb2a1bc5b6c4d5e2e077
ThembaMtshelwane <thembamm3@gmail.com>
2024-08-08 11:06:45 +0200

    Pages: Created homepage, series page and movies pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import Navbar from "./components/Navbar/Navbar";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+
+import HomePage from "./pages/HomePage"; // Home page
 
 export default function App() {
-  return (
-    <>
-      <Navbar />
-    </>
-  );
+  const router = createBrowserRouter(
+    createRoutesFromElements(<Route index element={<HomePage />} />)
+  ); // Creates router
+  return <RouterProvider router={router} />;
 }
