b67e7087ccc9b386bae9ff10f87db5802c8cf413
Themba Mtshelwane <Thembamm3@gmail.com>
2024-08-12 21:50:14 +0200

    Add Media:  Centered image upload label, added countries list and added image input

--- src/components/AddMedia.tsx
+++ src/components/AddMedia.tsx
@@ -1,21 +1,62 @@
-import { useState } from 'react'
-import {} from 'react-icons'
+import { useEffect, useState } from 'react'
+import { FaPlus } from 'react-icons/fa'
 
+interface CountryData {
+  name: { common: string }
+  code: string
+}
 type Props = {}
 const AddMedia = (props: Props) => {
   const [imageData, setImageData] = useState(null)
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    const fetchSingleMedia = async () => {
+      try {
+        const res = await fetch(`https://restcountries.com/v3.1/all`)
+        const data = await res.json()
+        setCountries(data)
+        console.log(data)
+      } catch (error) {
+        console.log('Error fetching data', error)
+      } finally {
+        // setIsLoading(false)
+      }
+    }
+    fetchSingleMedia()
+  }, [])
+
+  countries.sort((a: CountryData, b: CountryData) => {
+    const nameA = a.name.common.toUpperCase() // Convert to uppercase to ensure case-insensitive sorting
+    const nameB = b.name.common.toUpperCase()
+
+    if (nameA < nameB) {
+      return -1 // a comes before b
+    }
+    if (nameA > nameB) {
+      return 1 // a comes after b
+    }
+    return 0 // names are equal
+  })
+  const countriesOptions = countries.map((country: CountryData) => {
+    return (
+      <option key={country.name.common} value={country.name.common}>
+        {country.name.common}
+      </option>
+    )
+  }, [])
   return (
-    <section className="border-2 border-blue-600 mx-auto w-[90%] grid grid-cols-1 grid-rows-[1fr_1fr] my-10  sm:grid-cols-2 sm:grid-rows-1 md:w-[70%] ">
-      <section className="border-[5px] border-green-800">
+    <section className="border-2 border-blue-600 mx-auto w-[90%] h-fit grid grid-cols-1 grid-rows-[1fr_1fr] my-10  sm:grid-cols-2 sm:grid-rows-1 sm:w-[90%] lg:w-[70%] ">
+      <section className="border-[5px] border-green-800 h-[90%]">
         {imageData ? (
           <img src="" alt="" />
         ) : (
-          <section className="border-2 h-full grid grid-cols-8 grid-rows-[1.5fr_0.5fr_0.5fr_1.5fr] border-red-700">
+          <section className="border-2 h-full flex flex-col items-center justify-center">
             <h1 className="text-4xl font-bold grid items-center col-[2/8] row-[2/3]">
               Upload Image
             </h1>
-            <button className="text-[50px] border-2 rounded-full col-[4/6] row-[3/4] text-white bg-button">
-              +
+            <button className="general-button m-5">
+              <FaPlus />
             </button>
           </section>
         )}
@@ -25,6 +66,7 @@ const AddMedia = (props: Props) => {
           <p>Movie/Series name</p>
           <input type="text" placeholder="Movies/ Series name" />
         </label>
+        <input className="hidden" type="file" />
         <label htmlFor="">
           <p>Description</p>
           <textarea
@@ -35,8 +77,7 @@ const AddMedia = (props: Props) => {
         <label htmlFor="">
           <p>Country</p>
           <select name="" id="">
-            <option value="">South Africa</option>
-            <option value="">United States of America</option>
+            {countriesOptions}
           </select>
         </label>
         <label htmlFor="">
