8ba605f752217b4f4410e4555d40cfce7f625d5b
ThembaMtshelwane <thembamm3@gmail.com>
2024-08-14 11:45:08 +0200

    Utils: CRUD operations in utils file and added notifications

--- src/App.tsx
+++ src/App.tsx
@@ -13,83 +13,9 @@ import NotFoundPage from "./pages/NotFoundPage";
 import AddPage from "./pages/AddPage";
 import SingleMediaPage, { mediaLoader } from "./pages/SingleMediaPage";
 import EditPage from "./pages/EditPage";
-import { MediaData } from "./definitions";
-import { notType } from "./utils";
+import { addMedia, deleteMedia, editMedia } from "./utils";
 
 export default function App() {
-  // Add media
-  const addMedia = async (newMedia: MediaData) => {
-    let serverURL = `http://localhost:8080/${newMedia.type}`;
-    if (newMedia.type === "movie") {
-      serverURL = `http://localhost:8080/movies`;
-    }
-    try {
-      const res = await fetch(serverURL, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(newMedia),
-      });
-      if (!res.ok) {
-        throw new Error("Failed to add media");
-      }
-      alert("Media added successfully");
-    } catch (error) {
-      console.error(error);
-      alert("Failed to add media");
-    }
-    return;
-  };
-
-  //delete media
-  const deleteMedia = async (id: string, type: string) => {
-    let serverURL = `http://localhost:8080/${type}/${id}`;
-    if (type === "movie") {
-      serverURL = `/api/movies/${id}`;
-    }
-    const res = fetch(serverURL, {
-      method: "DELETE",
-    });
-    return;
-  };
-
-  const editMedia = async (updatedMedia: MediaData) => {
-    let serverURL = `http://localhost:8080/${updatedMedia.type}/${updatedMedia.id}`;
-    if (updatedMedia.type === "movie") {
-      serverURL = `http://localhost:8080/movies/${updatedMedia.id}`;
-    }
-
-    try {
-      // Check if the media exists at the specified endpoint
-      const checkRes = await fetch(serverURL);
-      if (checkRes.ok) {
-        // Media exists, proceed to update
-        const updateRes = await fetch(serverURL, {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(updatedMedia),
-        });
-
-        if (!updateRes.ok) {
-          throw new Error("Failed to update media");
-        }
-      } else {
-        // Media does not exist, handle accordingly
-        console.warn("Media does not exist at the specified endpoint.");
-        await addMedia(updatedMedia);
-        await deleteMedia(updatedMedia.id, notType(updatedMedia.type));
-      }
-    } catch (error) {
-      console.error("An error occurred while editing media:", error);
-      // If any error occurs, fallback to deleting from the old type and adding to the new type
-      await deleteMedia(updatedMedia.id, notType(updatedMedia.type));
-      await addMedia(updatedMedia);
-    }
-  };
-
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-import { CountryData } from "./definitions";
+import { CountryData, MediaData } from "./definitions";
 
 export const sortCountries = (list: CountryData[]) => {
   list.sort((a: CountryData, b: CountryData) => {
@@ -30,4 +30,90 @@ export const generateYearRange = () => {
 export const notType = (type: string) => {
   if (type === "movie") return "series";
   return "movie";
+};
+
+// Add media
+export const addMedia = async (newMedia: MediaData) => {
+  let serverURL = `http://localhost:8080/${newMedia.type}`;
+  if (newMedia.type === "movie") {
+    serverURL = `http://localhost:8080/movies`;
+  }
+  try {
+    const res = await fetch(serverURL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newMedia),
+    });
+    if (!res.ok) {
+      throw new Error("Failed to add media");
+    }
+    alert("Media added successfully");
+  } catch (error) {
+    console.error(error);
+    alert("Failed to add media");
+  }
+  return;
+};
+
+//delete media
+export const deleteMedia = async (id: string, type: string) => {
+  let serverURL = `http://localhost:8080/${type}/${id}`;
+  if (type === "movie") {
+    serverURL = `/api/movies/${id}`;
+  }
+
+  try {
+    const res = await fetch(serverURL, {
+      method: "DELETE",
+    });
+    if (!res.ok) {
+      throw new Error("Failed to add media");
+    }
+    alert("Media deleted successfully");
+  } catch (error) {
+    console.error(error);
+    alert("Failed to delete media");
+  }
+
+  return;
+};
+
+export const editMedia = async (updatedMedia: MediaData) => {
+  let serverURL = `http://localhost:8080/${updatedMedia.type}/${updatedMedia.id}`;
+  if (updatedMedia.type === "movie") {
+    serverURL = `http://localhost:8080/movies/${updatedMedia.id}`;
+  }
+
+  try {
+    // Check if the media exists at the specified endpoint
+    const checkRes = await fetch(serverURL);
+    if (checkRes.ok) {
+      // Media exists, proceed to update
+      const updateRes = await fetch(serverURL, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedMedia),
+      });
+
+      if (!updateRes.ok) {
+        throw new Error("Failed to update media");
+      }
+      alert("Media deleted successfully");
+    } else {
+      // Media does not exist, handle accordingly
+      console.warn("Media does not exist at the specified endpoint.");
+      await addMedia(updatedMedia);
+      await deleteMedia(updatedMedia.id, notType(updatedMedia.type));
+    }
+  } catch (error) {
+    console.error("An error occurred while editing media:", error);
+    // If any error occurs, fallback to deleting from the old type and adding to the new type
+    await deleteMedia(updatedMedia.id, notType(updatedMedia.type));
+    await addMedia(updatedMedia);
+    alert("Failed to edit media");
+  }
 };
