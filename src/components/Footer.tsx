7336411a1ff736184a86e8af651edc5e9b422e0c
ThembaMtshelwane <thembamm3@gmail.com>
2024-08-14 12:30:55 +0200

    Footer: Aligning links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,15 +4,16 @@ import Logo from "./Navbar/Logo";
 
 const Footer = () => {
   return (
-    <footer className=" bg-[#D9D9D9] text-lg h-fit  w-full p-10 grid items-center      sm:grid-cols-3  sm:text-xl md:text-xl">
+    <footer className=" bg-[#D9D9D9] text-lg h-fit  w-full p-4 mt-40 grid items-center sm:grid-cols-3  sm:text-xl md:text-xl">
       <Logo
-        styles="py-2 px-2 my-4 border-2 border-black w-full sm:w-fit"
+        styles="py-2 px-2 my-4 border-2 border-black w-full  ml-[15%] sm:w-fit sm:border-0"
         logoStyle={""}
       />
-      <Links styles="w-full justify-self-center sm:max-w-xs sm:grid sm:grid-cols-2" />
+      <Links styles="justify-items-center w-full justify-self-center sm:max-w-xs sm:grid sm:grid-cols-2" />
       <Link
         to="/subscibe"
-        className="uppercase sm:ml-auto sm:mr-[15%] py-2 px-2 my-4 border-2 border-black"
+        // className="uppercase sm:ml-auto sm:mr-[15%] py-2 px-2 my-4  "
+        className="general-button sm:ml-auto sm:mr-[15%]"
       >
         subscribe
       </Link>
